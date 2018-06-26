50dadd0c04391dddb0bfdf4da91c2d0bd4cd44b8
Carmen Shing <carmenshing@gmail.com>
2018-06-26 23:04:01 +0000

    modified to asCalback method, and commented on promise way

--- add_person_knex.js
+++ add_person_knex.js
@@ -3,7 +3,6 @@ const moment = require("moment");
 
 const knex = require('knex')({
     client: 'pg',
-    version: '7.2',
     connection: {
         host     : settings.hostname,
         user     : settings.user,
@@ -16,8 +15,14 @@ const firstName = process.argv[2];
 const lastName = process.argv[3];
 const date = process.argv[4];
 
-knex.select('first_name', 'last_name', 'birthdate').from('famous_people')
-.insert({first_name: firstName, last_name: lastName, birthdate: date}).into('famous_people')
-.catch(function(error) {
-    console.error(error)
-});
+knex('famous_people').insert({first_name: firstName, last_name: lastName, birthdate: date})
+    // asCallback way:
+    .asCallback((err, rows) => {
+        if (err) return console.error(err);
+        return knex.destroy();
+    });
+
+    // promise way:
+    // .catch(function(error) {
+    //     console.error(error)
+    // });
