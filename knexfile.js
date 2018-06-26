aa683aca2a440db4bb23f44a06144b2c59da835d
Carmen Shing <carmenshing@gmail.com>
2018-06-26 23:27:46 +0000

    updated config for development

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      filename: './dev.pg'
+      host     : settings.hostname,
+      user     : settings.user,
+      password : settings.password,
+      database : settings.database
     }
   },
 
